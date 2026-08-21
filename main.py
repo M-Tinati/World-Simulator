8a11f522b88976eacd369ea4fec9bb49a044a71a
M-Tinati <tinatigithub@gmail.com>
2026-08-21 12:29:42 +0330

    create world

---
+++ main.py
@@ -0,0 +1,12 @@
+World_Name = "Earth"
+Population = 10
+Temperature = 24.5
+World_Active = True
+
+print("============")
+print("World : " , World_Name )
+print("Population : " , Population )
+print("Temperature : " , Temperature )
+print("Active : " , World_Active )
+print("============")
+
