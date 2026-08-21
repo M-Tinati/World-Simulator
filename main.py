f3f2214559e60e448cd7e8abae607faa82b983b8
M-Tinati <tinatigithub@gmail.com>
2026-08-21 13:24:20 +0330

    change type bool

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-# World_Name = "Earth"
-# Population = 10
 Temperature = 24.5
-World_Active = True
+
 
 print("========== WORLD SIMULATOR ==========")
 World_Name = input("Enter world name : ")
 Creature_Name = input("Enter creature name : ")
 Population = int(input("Enter Population  : "))
+World_Active = int(input("Is the game active?"))
 print("========== WORLD CREATED ==========")
 print("World : " ,World_Name)
 print("First Creature:" ,  Creature_Name)
 print("Population : " , Population )
 print("Temperature : " , Temperature )
-print("Active : " , World_Active )
+print("Active : " , bool(World_Active) )
 print("Population Next Day:" , Population + 1)
 print("============")
 
