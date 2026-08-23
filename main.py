6ea6d872f0c1c8ab71a8862d83d7946b2470a998
M-Tinati <tinatigithub@gmail.com>
2026-08-23 21:23:17 +0330

    update

--- main.py
+++ main.py
@@ -102,3 +102,44 @@ for creature in creatures:
 
 print()
 print("========== SIMULATION END ==========")
+
+
+
+print("========== WORLD SIMULATOR ==========")
+
+creatures = []
+
+count = int(input("How many creatures? "))
+
+for i in range(count):
+    name = input("Enter creature name: ")
+    age = int(input("Enter creature age: "))
+    health = int(input("Enter creature health: "))
+
+    creature = {
+        "name": name,
+        "age": age,
+        "health": health
+    }
+
+    creatures.append(creature)
+
+print()
+print("========== CREATURES ==========")
+
+for creature in creatures:
+    if creature["health"] >= 70:
+        status = "Healthy"
+    elif creature["health"] >= 30:
+        status = "Weak"
+    else:
+        status = "Critical"
+
+    print()
+    print("Creature:", creature["name"])
+    print("Age:", creature["age"])
+    print("Health:", creature["health"])
+    print("Status:", status)
+
+print()
+print("========== SIMULATION END ==========")
