c41e49cc31c2e9c8153df43d471261eb889ac49e
M-Tinati <tinatigithub@gmail.com>
2026-08-30 19:52:03 +0330

    update

--- train.py
+++ train.py
@@ -1,13 +1,9 @@
-Creatures = [
-    {"name": "Wolf", "health": 80},
-    {"name": "Cat", "health": 20},
-    {"name": "Human", "health": 90},
-    {"name": "Monster", "health": 5}
-]
-new_creatures = Creatures.copy()
-for i in new_creatures:
-    if i["health"] <=30:
-        Creatures.remove(i)
-print(Creatures)
-print("===================")
-print(new_creatures)
+def show_status(Health):
+    if Health >= 70:
+        print("Healthy")
+    elif Health >= 30:
+        print("Weak")
+    else:
+        print("Critical")
+    
+show_status(80)
